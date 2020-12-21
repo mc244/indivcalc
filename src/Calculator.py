4ec62bcd30b6f11dff46b682e2801b3528fbe9e2
mc244 <mc244@njit.edu>
2020-12-21 17:16:43 -0500

    Feature: Add multiplication method and test.

--- src/Calculator.py
+++ src/Calculator.py
@@ -12,16 +12,27 @@ def subtraction(a, b):
     return c
 
 
+def multiply(a, b):
+    a = int(a)
+    b = int(b)
+    c = a * b
+    return c
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
 
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
 
-    def add(self, a, b):
-        self.result = addition(a, b)
+    def multiply(self, a, b):
+        self.result = multiply(a, b)
         return self.result
