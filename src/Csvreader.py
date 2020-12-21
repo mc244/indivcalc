8b426c9bd8f3746daaa0239fd2beb578e5889343
mc244 <mc244@njit.edu>
2020-12-21 16:57:01 -0500

    Fix: Added def ClassFactory and def return_data_as_class.

--- src/Csvreader.py
+++ src/Csvreader.py
@@ -2,12 +2,24 @@ import csv
 from pprint import pprint
 
 
+def ClassFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
+
+
 class Csvreader:
     data = []
 
     def __init__(self, filepath):
+        self.data = []
+
         with open(filepath) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
         pass
+
+    def return_data_as_class(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(ClassFactory(class_name, row))
+        return objects
