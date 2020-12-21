29696a86b38dccf6bc0b1b1ba6c234f43ad517b6
mc244 <mc244@njit.edu>
2020-12-21 16:57:18 -0500

    Feature: Add subtraction method and test.

--- src/Calculator.py
+++ src/Calculator.py
@@ -5,11 +5,22 @@ def addition(a, b):
     return c
 
 
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
 
     def add(self, a, b):
         self.result = addition(a, b)
