1c757fa1615e47984d497b670d81f49c6cb48a37
mc244 <mc244@njit.edu>
2020-12-21 09:00:15 -0500

    Fix: Renamed files with capital letter.

--- src/Calculatortests.py
+++ src/Calculatortests.py
@@ -1,11 +1,11 @@
 import unitest
-from Calculator import calculator
+from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calculator(self):
-        calculator = calculator()
-        self.assertIsInstance(calculator, calculator)
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
 
 if _name_== '_main_':
     unittest.main()
