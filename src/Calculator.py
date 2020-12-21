d200dd3d1cc9b0f0f30a21c27dadec70cd2b9c16
mc244 <mc244@njit.edu>
2020-12-21 14:29:38 -0500

    Feature: Add addition method and function.

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,19 @@
+from Csvreader import Csvreader
+
+
+def addition(a, b):
+    a = int(a)
+    b = int(b)
+    c = a + b
+    return c
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
